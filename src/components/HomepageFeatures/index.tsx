6b31213d078e45f0d41127e5596b61c0988069af
Issac Liu <issac@matchaflavored.site>
2024-09-23 03:38:52 -0400

    Remove blog posts and tags; update homepage installation instructions

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,49 +1,49 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
-type FeatureItem = {
+type InstallMethodItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
 };
 
-const FeatureList: FeatureItem[] = [
+const InstallMethodList: InstallMethodItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Windows",
+    Svg: require("@site/static/img/windows.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Installing MultiSim on Windows is the most simple process, as it's the
+        platform MultiSim is made for. Just download the installer and run it.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Apple Silicon",
+    Svg: require("@site/static/img/apple-silicon.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Installing MultiSim on Apple Silicon is slightly more complicated, but
+        we have a guide to help you through the installation.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Intel Macs",
+    Svg: require("@site/static/img/apple-intel.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Installing MultiSim on Intel Macs is only slightly more complicated than
+        installing on Windows, and we will have a guide for that too soon!
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function InstallMethods({ title, Svg, description }: InstallMethodItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -60,8 +60,8 @@ export default function HomepageFeatures(): JSX.Element {
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+          {InstallMethodList.map((props, idx) => (
+            <InstallMethods key={idx} {...props} />
           ))}
         </div>
       </div>
